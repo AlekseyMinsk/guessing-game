6f143f73cbeadcc6e15f043e30c601aa15d0be74
Aleksey Budanov <a.budanov@mageworx.com>
2019-10-10 15:57:49 +0300

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor(current = null, min = null, max = null, guess = null) {
+        this.current = current;
+        this.min = min;
+        this.max = max;
+        this.guessNumber = guess;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
+    	let halfDifference = Math.ceil((this.max - this.min) / 2);
+    	this.current = this.min + halfDifference;
+    	return this.current;
 
     }
 
     lower() {
-
+    	this.max = this.current;
     }
 
     greater() {
-
+		this.min = this.current;
     }
 }
 
